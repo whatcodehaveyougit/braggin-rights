d2f5b16b8cf55a6a46e66a365f809476877b617c
Jamie Ryan <56791368+MrJamieRyan@users.noreply.github.com>
2020-03-09 15:27:44 +0000

    Form now posts to Database.

--- frontend/src/containers/Dashboard.js
+++ frontend/src/containers/Dashboard.js
@@ -13,7 +13,20 @@ class Dashboard extends Component {
   }
 
   handleContestSubmit(submittedContest) {
+    console.log(submittedContest)
+    fetch('http://localhost:8080/contests/', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        title: submittedContest.title
+      })
+    })
+
     submittedContest.id = Date.now();
+
     const updatedContests = [...this.state.contests, submittedContest];
     this.setState({
       contests: updatedContests
@@ -25,7 +38,7 @@ class Dashboard extends Component {
 
     fetch(url)
       .then(res => res.json())
-      .then(contests => this.setState({ contests: contests }))
+      .then(contests => this.setState({ contests: contests._embedded.contests }))
       .catch(err => console.error);
   }
 
